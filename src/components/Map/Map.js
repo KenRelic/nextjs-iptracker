47dd7618c0e161b5f29378a1390dbc5540f83048
kenrelic <simeonkenneth3@gmail.com>
2021-01-24 20:57:16 +0100

    fix icon error on build

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -18,8 +18,8 @@ const Map = ({ children, className, ...rest }) => {
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require('leaflet/dist/images/marker-icon2-2x.png'),
-      iconUrl: require('leaflet/dist/images/marker-icon2.png'),
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
       shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
     });
   });
